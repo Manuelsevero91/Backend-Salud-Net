8086f12b668413fb50b4c0c61fb35ac5099b9202
karinadesposito <110677129+karinadesposito@users.noreply.github.com>
2023-08-21 12:15:54 -0300

    main listo para agregar ramas

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppService } from './app.service';
-import { MedicoController } from './medico/medico.controller';
-import { MedicoService } from './medico/medico.service';
+
 
 @Module({
   imports: [],
-  controllers: [AppController, MedicoController],
-  providers: [AppService, MedicoService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
