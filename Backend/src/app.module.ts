b3aeee09192829b3713c3431e4ef4c93c1a1e924
imolinap9191 <imolinap91@gmail.com>
2023-08-22 19:33:49 -0300

    patients

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DoctorsController } from './doctors/doctors.controller';
 import { DoctorsService } from './doctors/doctors.service';
+import { PatientsController } from './patients/patients.controller';
+import { PatientsService } from './patients/patients.service';
+import { PatientsModule } from './patients/patients.module';
+import { DoctorsModule } from './doctors/doctors.module';
 @Module({
-  imports: [],
-  controllers: [DoctorsController],
-  providers: [DoctorsService],
+  imports: [PatientsModule, DoctorsModule],
+  controllers: [DoctorsController, PatientsController],
+  providers: [DoctorsService, PatientsService],
 })
 export class AppModule {}
